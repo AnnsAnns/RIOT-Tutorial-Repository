4465bc4a6eef2a926a281268005ea4cd74e67cfd
AnnsAnn <git@annsann.eu>
2024-10-09 13:51:02 +0200

    Remove double \n

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 #![no_std]
 
-use riot_wrappers::riot_main;
 use riot_wrappers::println;
+use riot_wrappers::riot_main;
 
 extern crate rust_riotmodules;
 
 riot_main!(main);
 
 fn main() {
-    println!("You are running RIOT using Rust on a(n) {} board.\n", riot_wrappers::BOARD);
+    println!(
+        "You are running RIOT using Rust on a(n) {} board.",
+        riot_wrappers::BOARD
+    );
 }
